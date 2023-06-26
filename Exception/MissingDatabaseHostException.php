a836057b4f0ba8507119b028d2eddbb61c484468
supergnaw <loren.supernaw@gmail.com>
2023-06-26 16:08:37 -0500

    Add exception files via upload

---
+++ Exception/MissingDatabaseHostException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\Nestbox\Exception;
+
+class MissingDatabaseHostException extends NestboxException
+{
+}
---
+++ Exception/TransactionRollbackFailedException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\Nestbox\Exception;
+
+class TransactionRollbackFailedException extends NestboxException
+{
+}
